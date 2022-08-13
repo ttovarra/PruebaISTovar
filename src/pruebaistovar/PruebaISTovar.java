e8a6f99f8ff0736c5f9ee06553f9a192e73ef843
beto <ttovarra@gmail.com>
2022-08-13 09:07:56 -0500

    Prueba IS TOVAR

---
+++ src/pruebaistovar/PruebaISTovar.java
@@ -0,0 +1,14 @@
+
+package pruebaistovar;
+
+
+public class PruebaISTovar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Prueba GITHUB TOVAR");
+    }
+    
+}
